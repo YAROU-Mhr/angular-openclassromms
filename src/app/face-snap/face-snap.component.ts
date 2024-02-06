844dd2ede3bcee194dacca0484c672d4c4a1f93b
YAROU MHR <kingmhr39@gmail.com>
2024-02-06 15:31:30 +0100

    Réaction au évènement avec onclick terminé, ajout de propriété personnalisé en tammé
    Création du model qui sa servir de classe a implémenter déjà éffectué (face-snap-model.ts)

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -11,12 +11,25 @@ export class FaceSnapComponent implements OnInit {
   createdDate!: Date;
   snaps!: number;
   imageUrl!: string;
+  btnText!: string;
 
   ngOnInit(): void {
     this.title = 'YAROU';
     this.description = 'Dévéloppeur FullStack';
     this.createdDate = new Date();
     this.snaps = 5;
+    this.btnText = "Oh add Snap";
     this.imageUrl = "https://yaroumhr.website/storage/about/thumbnails/I9PB6XKCyICkp65erD0ZYhrW6ElhQN-metaTUhSLnN2Zw==-.svg";
   }
+
+  onSnaps() {
+    if (  this.btnText === "Oh remove Snap!") {
+      this.snaps--;
+      this.btnText = "Oh add Snap!";
+    } else {
+      this.snaps++;
+      this.btnText = "Oh remove Snap!";
+    }
+  }
+
 }
