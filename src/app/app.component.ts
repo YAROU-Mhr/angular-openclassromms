b7865e034caabdfd6f029b0f26aca835891e9475
YAROU MHR <kingmhr39@gmail.com>
2024-02-06 18:06:37 +0100

    Fin: Affichez des listes avec boucle *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,36 +8,33 @@ import { FaceSnap } from './models/face-snap.model';
 })
 export class AppComponent {
 
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLatestSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit() {
-    this.mySnap = {
-      title:'Archibald',
-      description:'Mon meilleur ami depuis tout petit !',
-      createdDate:new Date(),
-      snaps:0,
-      imageUrl:'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      location:'Port-Novo',
-    };
-
-    this.myOtherSnap = {
-      title:'YAROU MHR',
-      description:'Dévéloppeur FullStack',
-      createdDate:new Date(),
-      snaps:0,
-      imageUrl:'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      location:'Cotonou',
-    };
-
-    this.myLatestSnap = {
-      title:'Bref',
-      description:'Mon meilleur ami depuis tout petit !',
-      createdDate:new Date(),
-      snaps:10,
-      imageUrl:'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
-    };
-
+    this.faceSnaps = [
+      {
+        title: 'Archibald',
+        description: 'Mon meilleur ami depuis tout petit !',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        location: 'Port-Novo',
+      },
+      {
+        title: 'YAROU MHR',
+        description: 'Dévéloppeur FullStack',
+        createdDate: new Date(),
+        snaps: 0,
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        location: 'Cotonou',
+      },
+      {
+        title: 'Bref',
+        description: 'Mon meilleur ami depuis tout petit !',
+        createdDate: new Date(),
+        snaps: 10,
+        imageUrl: 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg'
+      },
+    ];
   }
 }
